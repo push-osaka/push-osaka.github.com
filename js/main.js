b9e8ce7b77a54031e582e3b51c3d8430cab80629
earthmaking <kimring2@gmail.com>
2014-09-03 17:35:42 +0900

    アコーディオン処理変更

--- js/main.js
+++ js/main.js
@@ -146,10 +146,10 @@ $(function() {
 				   		"<div class='accordion-heading'>" +
 							"<div class='pushOsakaDate'>" + data[i].date.value.replace("T", " ").replace("Z", "") +"</div>" +
 							"<div class='pushOsakaCategoryLabel'>" + data[i].category_label.value +"</div>" +
-							"<div class='accordion-table'><img src='img/" + svgCategory +"' width='30'></div>" +
 							"<a class='accordion-toggle' data-toggle='collapse' data_parent='#rss-list' href='#rss" + i + "'>" +
 								"<div class='accordion-table pushOsaka'>" + data[i].title.value +  "</div>" +
 							"</a>" +
+							"<div class='accordion-table'><img src='img/" + svgCategory +"' width='30'></div>" +
 						"</div>" +
 						"<div id='rss" + i +"' class='according-body collapse'>" +
 							"<div class='accordion-inner'>" +
