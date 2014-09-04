b325647b2d7d09f732b157f884057665128d8b5a
earthmaking <kimring2@gmail.com>
2014-09-04 16:23:26 +0900

    子供オプション対応

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(function() {
 					filter += "regex(?key,'" + childTable[0][i] + "') ||";
 				}
 			}
-			filter = filter.substr(filter.length - 3);
+			filter = filter.substr(0, filter.length - 3);
 			filter += ")";
 		}
 		return filter;
